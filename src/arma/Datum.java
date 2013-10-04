2df5191deb5052fd956bc0edc6142ea3068b7b98
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-10-04 22:29:33 +0200

    Added some more constants

--- src/arma/Datum.java
+++ src/arma/Datum.java
@@ -2,4 +2,9 @@ package arma;
 
 public class Datum {
   public static final double pi = Math.PI;
+  public static final double inf = Double.POSITIVE_INFINITY;
+  public static final double nan = Double.NaN;
+  public static final double e = Math.E;
+  public static final double sqrt2 = Math.sqrt(2);
+  public static final double eps = Math.pow(2, -52);
 }
