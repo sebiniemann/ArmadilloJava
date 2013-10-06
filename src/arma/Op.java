037a39b991e1bbf03341832e058de4e8d59b0ffe
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-10-06 15:26:51 +0200

    Relational operators are now evaluated in arma.Op

--- src/arma/Op.java
+++ src/arma/Op.java
@@ -1,5 +1,7 @@
 package arma;
 
+import org.ejml.data.DenseMatrix64F;
+
 /**
  * <p>
  * Provides constants for the following operators.
@@ -60,5 +62,146 @@ public enum Op {
   /**
    * Non-strict greater than (relational operator)
    */
-  GREATER
+  GREATER;
+
+  /**
+   * <b>Non-canonical:</b> Creates a column vector containing a {@code 1} for each element for which an element-wise evaluation of
+   * {@code a operator b} holds true and a {@code 0} otherwise.
+   * 
+   * @param a The left-hand side operand.
+   * @param operator The operator to be used.
+   * @param b The right-hand side operand.
+   * @return The created column vector.
+
+   * @throws UnsupportedOperationException <b>Non-canonical:</b> Thrown if another operator besides relational operators
+   *           is provided.
+   * @throws IllegalArgumentException <b>Non-canonical:</b> Thrown if both arguments are matrices but have an unequal
+   *           number of elements.
+   */
+  public static Mat evaluate(Mat a, Op operator, Mat b) throws UnsupportedOperationException, IllegalArgumentException {
+    if (a.n_elem != b.n_elem) {
+      throw new IllegalArgumentException("The provided matrices have an unequal number of elements (" + a.n_elem + " != " + b.n_elem + ").");
+    }
+    
+    DenseMatrix64F result = new DenseMatrix64F(a.n_elem, 1);
+    DenseMatrix64F memptrA = a.memptr();
+    DenseMatrix64F memptrB = b.memptr();
+
+    for (int i = 0; i < a.n_elem; i++) {
+      if (evaluate(memptrA.get(i), operator, memptrB.get(i))) {
+        result.set(a.convertMajorOrdering(i), 1);
+      }
+    }
+
+    return new Mat(result);
+  }
+
+  /**
+   * <b>Non-canonical:</b> Creates a column vector containing a {@code 1} for each element for which an element-wise evaluation of
+   * {@code a operator b} holds true and a {@code 0} otherwise.
+   * 
+   * @param a The left-hand side operand.
+   * @param operator The operator to be used.
+   * @param b The right-hand side operand.
+   * @return The created column vector.
+
+   * @throws UnsupportedOperationException <b>Non-canonical:</b> Thrown if another operator besides relational operators
+   *           is provided.
+   *           
+   * @see #evaluate(Mat, Op, Mat)
+   */
+  public static Mat evaluate(Mat a, Op operator, double b) throws UnsupportedOperationException {
+    DenseMatrix64F result = new DenseMatrix64F(a.n_elem, 1);
+    DenseMatrix64F memptrA = a.memptr();
+
+    for (int i = 0; i < a.n_elem; i++) {
+      if (evaluate(memptrA.get(i), operator, b)) {
+        result.set(a.convertMajorOrdering(i), 1);
+      }
+    }
+
+    return new Mat(result);
+  }
+
+  /**
+   * <b>Non-canonical:</b> Creates a column vector containing a {@code 1} for each element for which an element-wise evaluation of
+   * {@code a operator b} holds true and a {@code 0} otherwise.
+   * 
+   * @param a The left-hand side operand.
+   * @param operator The operator to be used.
+   * @param b The right-hand side operand.
+   * @return The created column vector.
+
+   * @throws UnsupportedOperationException <b>Non-canonical:</b> Thrown if another operator besides relational operators
+   *           is provided.
+   *           
+   * @see #evaluate(Mat, Op, Mat)
+   */
+  public static Mat evaluate(double a, Op operator, Mat b) throws UnsupportedOperationException {
+    switch (operator) {
+      case STRICT_LESS:
+        return evaluate(b, Op.STRICT_GREATER, a);
+      case LESS:
+        return evaluate(b, Op.GREATER, a);
+      case GREATER:
+        return evaluate(b, Op.LESS, a);
+      case STRICT_GREATER:
+        return evaluate(b, Op.STRICT_LESS, a);
+      case EQUAL:
+      case NOT_EQUAL:
+        return evaluate(b, operator, a);
+      default:
+        throw new UnsupportedOperationException("Only relational operators are supported.");
+    }
+  }
+
+  /**
+   * Returns true if the evaluation of {@code a operator b} holds true.
+   * 
+   * @param a The left-hand side operand.
+   * @param operator The operator to be used.
+   * @param b The right-hand side operand.
+   * @return The boolean result.
+   * 
+   * @throws UnsupportedOperationException <b>Non-canonical:</b> Thrown if another operator besides relational operators
+   *           is provided.
+   */
+  private static boolean evaluate(double a, Op operator, double b) throws UnsupportedOperationException {
+    switch (operator) {
+      case STRICT_LESS:
+        if (a < b) {
+          return true;
+        }
+        break;
+      case LESS:
+        if (a <= b) {
+          return true;
+        }
+        break;
+      case EQUAL:
+        if (a == b) {
+          return true;
+        }
+        break;
+      case NOT_EQUAL:
+        if (a != b) {
+          return true;
+        }
+        break;
+      case GREATER:
+        if (a >= b) {
+          return true;
+        }
+        break;
+      case STRICT_GREATER:
+        if (a > b) {
+          return true;
+        }
+        break;
+      default:
+        throw new UnsupportedOperationException("Only relational operators are supported.");
+    }
+
+    return false;
+  }
 }
