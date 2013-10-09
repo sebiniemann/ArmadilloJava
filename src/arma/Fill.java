e5d00060eebb39516ae0f99f071e35f62c0aec3e
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-10-09 11:40:32 +0200

    Added enum Fill

---
+++ src/arma/Fill.java
@@ -0,0 +1,31 @@
+package arma;
+
+/**
+ * @author Sebastian Niemann <niemann@sra.uni-hannover.de>
+ */
+public enum Fill {
+  /**
+ * 
+ */
+  ZEROS,
+  /**
+ * 
+ */
+  ONES,
+  /**
+ * 
+ */
+  EYE,
+  /**
+ * 
+ */
+  RANDU,
+  /**
+ * 
+ */
+  RANDN,
+  /**
+ * 
+ */
+  NONE
+}
