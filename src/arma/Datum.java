33c16dac5dc18be5809ecd2f685e9dbd4d6607eb
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-10-09 11:40:44 +0200

    Added pre-defined constants

--- src/arma/Datum.java
+++ src/arma/Datum.java
@@ -3,7 +3,7 @@ package arma;
 /**
  * Provides pre-defined constants.
  * 
- * @author niemann
+ * @author Sebastian Niemann <niemann@sra.uni-hannover.de>
  */
 public class Datum {
   /**
@@ -30,4 +30,169 @@ public class Datum {
    * The machine epsilon.
    */
   public static final double eps = Math.pow(2, -52);
+  
+  /**
+   * 
+   */
+  public static final double log_min = Math.log(Double.MIN_NORMAL);
+  
+  /**
+   * 
+   */
+  public static final double log_max = Math.log(Double.MAX_VALUE);
+  
+  /**
+   * 
+   */
+  public static final double euler = 0.5772156649015329;
+  
+  /**
+   * 
+   */
+  public static final double gratio = 1.6180339887498948;
+  
+  /**
+   * 
+   */
+  public static final double m_u = 1.660538782e-27;
+  
+  /**
+   * 
+   */
+  public static final double N_A = 6.02214179e23;
+  
+  /**
+   * 
+   */
+  public static final double k = 1.3806504e-23;
+  
+  /**
+   * 
+   */
+  public static final double k_evk = 8.617343e-5;
+  
+  /**
+   * 
+   */
+  public static final double a_0 = 0.52917720859e-10;
+  
+  /**
+   * 
+   */
+  public static final double mu_B = 927.400915e-26;
+  
+  /**
+   * 
+   */
+  public static final double Z_0 = 3.76730313461771e-2;
+  
+  /**
+   * 
+   */
+  public static final double G_0 = 7.7480917004e-5;
+  
+  /**
+   * 
+   */
+  public static final double k_e = 8.9875517873681764e9;
+  
+  /**
+   * 
+   */
+  public static final double eps_0 = 8.85418781762039e-12;
+  
+  /**
+   * 
+   */
+  public static final double m_e = 9.10938215e-31;
+  
+  /**
+   * 
+   */
+  public static final double eV = 1.602176487e-19;
+  
+  /**
+   * 
+   */
+  public static final double ec = 1.602176487e-19;
+  
+  /**
+   * 
+   */
+  public static final double F = 96485.3399;
+  
+  /**
+   * 
+   */
+  public static final double alpha = 7.2973525376e-3;
+  
+  /**
+   * 
+   */
+  public static final double alpha_inv = 137.035999679;
+  
+  /**
+   * 
+   */
+  public static final double K_J = 483597.891e9;
+  
+  /**
+   * 
+   */
+  public static final double mu_0 = 1.25663706143592e-06;
+  
+  /**
+   * 
+   */
+  public static final double phi_0 = 2.067833667e-15;
+  
+  /**
+   * 
+   */
+  public static final double R = 8.314472;
+  
+  /**
+   * 
+   */
+  public static final double G = 6.67428e-11;
+  
+  /**
+   * 
+   */
+  public static final double h = 6.62606896e-34;
+  
+  /**
+   * 
+   */
+  public static final double h_bar = 1.054571628e-34;
+  
+  /**
+   * 
+   */
+  public static final double m_p = 1.672621637e-27;
+  
+  /**
+   * 
+   */
+  public static final double R_inf = 10973731.568527;
+  
+  /**
+   * 
+   */
+  public static final double c_0 = 299792458.0;
+  
+  /**
+   * 
+   */
+  public static final double sigma = 5.670400e-8;
+  
+  /**
+   * 
+   */
+  public static final double R_k = 25812.807557;
+  
+  /**
+   * 
+   */
+  public static final double b = 2.8977685e-3;
 }
