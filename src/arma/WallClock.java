1b79489e384ca76225f1fdfd345195da5414c3de
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-10-23 10:50:23 +0200

    More consistent comments (WallClock)

--- src/arma/WallClock.java
+++ src/arma/WallClock.java
@@ -12,26 +12,28 @@ package arma;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Measures the elapsed time in seconds.
+ * Measures the elapsed time between the last invoke {@link #tic()} and {@link #toc()} in seconds.
+ * <p>
+ * <b>Non-canonical:</b> The precision is bounded by {@code System.nanoTime()}
  * 
  * @author Sebastian Niemann <niemann@sra.uni-hannover.de>
  */
 public class WallClock {
 
   /**
-   * The starting time.
+   * The start instant of a time interval.
    */
   private static long _tic;
 
   /**
-   * Sets the starting time to the current time.
+   * Sets the start instant of a time interval to the current instant.
    */
   public static void tic() {
     _tic = System.nanoTime();
   }
 
   /**
-   * Returns the elapsed time between the current time and the stating time.
+   * Returns the elapsed time between the last invoke {@link #tic()} and the current instant.
    * 
    * @return The elapsed time.
    */
