175603df65bd120559bbe943924a0450dace6785
dkiechle <SRA@daniels-mbp-3.fritz.box>
2013-11-18 20:30:53 +0100

    Fill Statistic test class

--- test/Arma/arma/TestScalarVectorValuedFunctionsOfVectorsMatricesStatistic.java
+++ test/Arma/arma/TestScalarVectorValuedFunctionsOfVectorsMatricesStatistic.java
@@ -21,6 +21,7 @@ import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runner.RunWith;
 
+
 /**
  * @author Daniel Kiechle <kiechle@sra.uni-hannover.de>
  */
@@ -62,178 +63,344 @@ public class TestScalarVectorValuedFunctionsOfVectorsMatricesStatistic {
 
 	  /**
 	   * Test method for {@link arma.Arma#min(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMinMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMinMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.min.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.min(_testData.col(j)), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#minMat(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMinMatMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMinMatMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.min.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.minMat(_testData), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#minMat(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMinMatMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMinMatMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.min.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.minMat(_testData,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.min.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.minMat(_testData,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#max(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMaxMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMaxMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.max.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.max(_testData.col(j)), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#maxMat(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMaxMatMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMaxMatMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.max.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.maxMat(_testData), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#maxMat(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMaxMatMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMaxMatMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.max.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.maxMat(_testData,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.max.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.maxMat(_testData,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#mean(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMeanMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMeanMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.mean.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.mean(_testData.col(j)), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#meanMat(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMeanMatMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMeanMatMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.mean.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.meanMat(_testData), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#meanMat(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMeanMatMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMeanMatMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.mean.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.meanMat(_testData,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.mean.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.meanMat(_testData,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#median(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMedianMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMedianMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.median.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.median(_testData.col(j)), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#medianMat(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMedianMatMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMedianMatMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.median.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.medianMat(_testData), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#medianMat(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testMedianMatMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testMedianMatMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.median.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.medianMat(_testData,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.median.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.medianMat(_testData,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#stddev(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testStddevMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testStddevMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n0.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.stddev(_testData.col(j)), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#stddev(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testStddevMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testStddevMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n0.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.stddev(_testData.col(j),0), TestUtil.NUMERIC_TOLERANCE);
+		  }
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n1.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.stddev(_testData.col(j),1), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#stddevMat(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testStddevMatMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testStddevMatMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n0.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#stddevMat(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testStddevMatMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testStddevMatMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n0.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n1.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#stddevMat(AbstractMat, int, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testStddevMatMatIntInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testStddevMatMatIntInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n0.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData,0,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n1.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData,1,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n0.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData,0,1), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.stddev.n1.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.stddevMat(_testData,1,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#var(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testVarMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testVarMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n0.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.var(_testData.col(j)), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#var(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testVarMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testVarMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n0.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.var(_testData.col(j),0), TestUtil.NUMERIC_TOLERANCE);
+		  }
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n1.d0.mat");
+
+		  for(int j = 0; j < _testData.n_cols; j++) {
+		    assertEquals(expected.at(j), Arma.var(_testData.col(j),1), TestUtil.NUMERIC_TOLERANCE);
+		  }
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#varMat(AbstractMat)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testVarMatMat() {
-	    fail("Not yet implemented"); // TODO
+	  public void testVarMatMat() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n0.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#varMat(AbstractMat, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testVarMatMatInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testVarMatMatInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n0.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n1.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 	  /**
 	   * Test method for {@link arma.Arma#varMat(AbstractMat, int, int)}.
+	   * @throws IOException 
 	   */
 	  @Test
-	  public void testVarMatMatIntInt() {
-	    fail("Not yet implemented"); // TODO
+	  public void testVarMatMatIntInt() throws IOException {
+		  Mat expected = new Mat();
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n0.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData,0,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n1.d0.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData,1,0), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n0.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData,0,1), TestUtil.NUMERIC_TOLERANCE);
+		  
+		  expected.load("test/data/Arma/TestScalarVectorValuedFunctionsOfVectorsMatrices/Statistic.var.n1.d1.mat");
+		  
+		  TestUtil.assertMatEquals(expected,Arma.varMat(_testData,1,1), TestUtil.NUMERIC_TOLERANCE);
 	  }
 
 }
