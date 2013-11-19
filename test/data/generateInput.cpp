0ed81db1d1956e1cfa3d8f3e186c21a99b26ba0e
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-11-20 00:26:01 +0100

    Added copyright information

--- test/data/generateInput.cpp
+++ test/data/generateInput.cpp
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright 2013 Sebastian Niemann <niemann@sra.uni-hannover.de> and contributors.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/MIT
+ *******************************************************************************/
 // http://arma.sourceforge.net/docs.html#config_hpp
 #define ARMA_USE_CXX11 // Use C++11 features, such as initialiser lists
 
