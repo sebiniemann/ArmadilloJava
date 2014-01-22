21b545f1ebe50eed274c79c31e139c4272a39e44
SebastianNiemann <niemann@sra.uni-hannover.de>
2014-01-22 16:03:26 +0100

    Added missing javadoc to AbstractVector

--- src/main/java/org/armadillojava/AbstractVector.java
+++ src/main/java/org/armadillojava/AbstractVector.java
@@ -90,31 +90,109 @@ abstract class AbstractVector extends AbstractMat {
    */
   abstract public void set_size(int n_elem);
 
+  /**
+   * Performs a in-place unary operation on the {@code first_index}th to {@code last_index} element.
+   * 
+   * @param first_index The first position
+   * @param last_index The last position
+   * @param unary_operator The unary operator
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code first_index}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code last_index}) is out of bounds.
+   */
   public void subvec(int first_index, int last_index, Op unary_operator) {
     rows(first_index, last_index, unary_operator);
   }
 
+  /**
+   * Performs a in-place binary operation on the {@code first_index}th to {@code last_index} element with the specified right-hand side operand.
+   * 
+   * @param first_index The first position
+   * @param last_index The last position
+   * @param binary_operator The binary operator
+   * @param operand The operand
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code first_index}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code last_index}) is out of bounds.
+   */
   public void subvec(int first_index, int last_index, Op binary_operator, double operand) {
     rows(first_index, last_index, binary_operator, operand);
   }
 
+  /**
+   * Performs a in-place binary operation on the {@code first_index}th to {@code last_index} element with the specified right-hand side operand.
+   * 
+   * @param first_index The first position
+   * @param last_index The last position
+   * @param binary_operator The binary operator
+   * @param operand The operand
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code first_index}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code last_index}) is out of bounds.
+   */
   public void subvec(int first_index, int last_index, Op binary_operator, AbstractMat operand) {
     rows(first_index, last_index, binary_operator, operand);
   }
 
+  /**
+   * Performs a in-place unary operation on the {@code span._first}th to {@code span._last} element.
+   * 
+   * @param span The span
+   * @param unary_operator The unary operator
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code span._first}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code span._last}) is out of bounds.
+   */
   public void subvec(Span span, Op unary_operator) {
     subvec(span._first, span._last, unary_operator);
   }
 
+  /**
+   * Performs a in-place binary operation on the {@code span._first}th to {@code span._last} element with the specified right-hand side operand.
+   * 
+   * @param span The span
+   * @param binary_operator The binary operator
+   * @param operand The operand
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code span._first}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code span._last}) is out of bounds.
+   */
   public void subvec(Span span, Op binary_operator, double operand) {
     subvec(span._first, span._last, binary_operator, operand);
   }
-
+  
+  /**
+   * Performs a in-place binary operation on the {@code span._first}th to {@code span._last} element with the specified right-hand side operand.
+   * 
+   * @param span The span
+   * @param binary_operator The binary operator
+   * @param operand The operand
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code span._first}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code span._last}) is out of bounds.
+   */
   public void subvec(Span span, Op binary_operator, AbstractMat operand) {
     subvec(span._first, span._last, binary_operator, operand);
   }
-
+  
+  /**
+   * Returns a deep copy of the {@code first_index}th to {@code last_index} element.
+   * 
+   * @param first_index The first position
+   * @param last_index The last position
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code first_index}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code last_index}) is out of bounds.
+   */
   abstract public AbstractVector subvec(int first_index, int last_index) throws IndexOutOfBoundsException;
-
+  
+  /**
+   * Returns a deep copy of the {@code span._first}th to {@code span._last} element.
+   * 
+   * @param span The span
+   * 
+   * @throws IndexOutOfBoundsException The first position ({@code span._first}) is out of bounds.
+   * @throws IndexOutOfBoundsException The last position ({@code span._last}) is out of bounds.
+   */
   abstract public AbstractVector subvec(Span span) throws IndexOutOfBoundsException;
 }
