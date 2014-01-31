3e878ac841e4e0c40b191e1cd11d282ab1099671
SebastianNiemann <niemann@sra.uni-hannover.de>
2014-01-31 23:25:56 +0100

    Added DistrParam for randi

---
+++ src/main/java/org/armadillojava/DistrParam.java
@@ -0,0 +1,14 @@
+package org.armadillojava;
+
+public class DistrParam {
+
+  protected int _a;
+  
+  protected int _b;
+  
+  public DistrParam(int a, int b) {
+    _a = a;
+    _b = b;
+  }
+  
+}
