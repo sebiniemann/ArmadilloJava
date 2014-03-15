65f87fe5dfdf785c6e62338cb736f1766735f7ad
SebastianNiemann <niemann@sra.uni-hannover.de>
2014-03-16 00:19:58 +0100

    Updated input generator

--- src/test/data/generateInput.cpp
+++ src/test/data/generateInput.cpp
@@ -15,9 +15,15 @@
 #define ARMA_USE_CXX11 // Use C++11 features, such as initialiser lists
 #include <iostream>
 // EXIT_SUCCESS
+using std::cout;
+using std::endl;
 
 #include <string>
 using std::to_string;
+
+#include <cmath>
+using std::pow;
+using std::abs;
 
 #include <armadillo>
 using arma::Mat;
@@ -26,23 +32,9 @@ using arma::ones;
 using arma::eye;
 using arma::raw_ascii;
 using arma::endr;
-using arma::join_horiz;
-using arma::join_vert;
 using arma::datum;
 
-Mat<double> getHankelMatrix(int numberOfRows, int numberOfColumns) {
-  Mat<double> hankelMatrix(numberOfRows, numberOfColumns);
-
-  for (int j = 0; j < numberOfColumns; j++) {
-    for (int i = 0; i < numberOfRows; i++) {
-      hankelMatrix.at(i, j) = i + j + 1;
-    }
-  }
-
-  return hankelMatrix;
-}
-
-Mat<double> getHilbertMatrix(int numberOfRows, int numberOfColumns) {
+Mat<double> generateHilbertMatrix(int numberOfRows, int numberOfColumns) {
   Mat<double> hilbertMatrix(numberOfRows, numberOfColumns);
 
   for (int j = 0; j < numberOfColumns; j++) {
@@ -54,44 +46,44 @@ Mat<double> getHilbertMatrix(int numberOfRows, int numberOfColumns) {
   return hilbertMatrix;
 }
 
-Mat<double> getNumberedMatrix(int numberOfRows, int numberOfColumns) {
-  Mat<double> numberedMatrix(numberOfRows, numberOfColumns);
+Mat<double> generateKMSMatrix(int numberOfRows, int numberOfColumns) {
+  Mat<double> KMSMatrix(numberOfRows, numberOfColumns);
 
   for (int j = 0; j < numberOfColumns; j++) {
     for (int i = 0; i < numberOfRows; i++) {
-      numberedMatrix.at(i, j) = i + j * numberedMatrix.n_rows;
+    	KMSMatrix.at(i, j) = pow(2, abs(i - j));
     }
   }
 
-  return numberedMatrix;
+  return KMSMatrix;
 }
 
 int main() {
   Mat<double> input;
 
-  double dimensions[] = {1, 2, 3, 4, 5, 10, 100};
+  double NumElems[] = {1, 2, 3, 4, 5, 10, 100, 1000, 10000};
+  double NumCols[] = {1, 2, 3, 4, 5, 10};
+  double NumRows[] = {1, 2, 3, 4, 5, 10};
 
-  for (int numberOfRows : dimensions) {
-    for (int numberOfColumns : dimensions) {
-      input = zeros<Mat<double>>(numberOfRows, numberOfColumns);
-      input.save("./input/zeros." + to_string(numberOfRows) + "x" + to_string(numberOfColumns) + ".mat", raw_ascii);
+  for (int n_cols : NumCols) {
+    for (int n_rows : NumRows) {
+      input = zeros<Mat<double>>(n_rows, n_cols);
+      input.save("./input/zeros." + to_string(n_rows) + "x" + to_string(n_cols) + ".mat", raw_ascii);
 
-      input = ones<Mat<double>>(numberOfRows, numberOfColumns);
-      input.save("./input/ones." + to_string(numberOfRows) + "x" + to_string(numberOfColumns) + ".mat", raw_ascii);
+      input = ones<Mat<double>>(n_rows, n_cols);
+      input.save("./input/ones." + to_string(n_rows) + "x" + to_string(n_cols) + ".mat", raw_ascii);
 
-      input = eye<Mat<double>>(numberOfRows, numberOfColumns);
-      input.save("./input/eye." + to_string(numberOfRows) + "x" + to_string(numberOfColumns) + ".mat", raw_ascii);
+      input = eye<Mat<double>>(n_rows, n_cols);
+      input.save("./input/eye." + to_string(n_rows) + "x" + to_string(n_cols) + ".mat", raw_ascii);
 
-      input = getNumberedMatrix(numberOfRows, numberOfColumns);
-      input.save("./input/numbered." + to_string(numberOfRows) + "x" + to_string(numberOfColumns) + ".mat", raw_ascii);
+      input = generateHilbertMatrix(n_rows, n_cols);
+      input.save("./input/hilbert." + to_string(n_rows) + "x" + to_string(n_cols) + ".mat", raw_ascii);
 
-      input = getHankelMatrix(numberOfRows, numberOfColumns);
-      input.save("./input/hankel." + to_string(numberOfRows) + "x" + to_string(numberOfColumns) + ".mat", raw_ascii);
+      input = generateHilbertMatrix(n_rows, n_cols) - 2.0 / (n_rows + n_cols + 2);
+      input.save("./input/hilbertSub." + to_string(n_rows) + "x" + to_string(n_cols) + ".mat", raw_ascii);
 
-      if (numberOfRows < 10 && numberOfColumns < 10) {
-        input = getHilbertMatrix(numberOfRows, numberOfColumns);
-        input.save("./input/hilbert." + to_string(numberOfRows) + "x" + to_string(numberOfColumns) + ".mat", raw_ascii);
-      }
+      input = generateKMSMatrix(n_rows, n_cols);
+      input.save("./input/ksm." + to_string(n_rows) + "x" + to_string(n_cols) + ".mat", raw_ascii);
     }
   }
 
@@ -99,56 +91,9 @@ int main() {
   input = join_vert(input, -input);
   input.save("./input/trigonometric.mat", raw_ascii);
 
-  input = {1, 2, 1/2.0 * datum::pi, datum::pi, 3/2.0 * datum::pi, 2 * datum::pi, 3 * datum::pi, 4 * datum::pi, 5 * datum::pi, 10 * datum::pi, 100 * datum::pi};
-  input.save("./input/trigonometric.acosh.mat", raw_ascii);
-	
-  input = {0, 1, datum::eps, 0.3, 0.6, 0.8, 0.9};
-  input = join_vert(input, -input);
-  input.save("./input/trigonometric.acos.asin.mat", raw_ascii);
-	
-  input = {0, datum::eps, 0.3, 0.6, 0.8, 0.9};
-  input = join_vert(input, -input);
-  input.save("./input/trigonometric.atanh.mat", raw_ascii);
-	
-  input = {1, 2, datum::eps, datum::inf, datum::e, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 0.9, 0.89, 0.789, 0.6789, 0.56789, 0.456789, 0.3456789, 0.23456789, 0.123456789};
-  input = join_vert(input, -input);
-  input.save("./input/miscellaneous.mat", raw_ascii);
-
-  input = {1, 2, datum::inf, datum::e, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 0.9, 0.89, 0.789, 0.6789, 0.56789, 0.456789, 0.3456789, 0.23456789, 0.123456789};
-  input.save("./input/miscellaneous.log.pow.mat", raw_ascii);
-	
-  input = {0, 1, 2, datum::inf, datum::e, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 0.9, 0.89, 0.789, 0.6789, 0.56789, 0.456789, 0.3456789, 0.23456789, 0.123456789};
-  input.save("./input/miscellaneous.sqrt.mat", raw_ascii);
-	
-  input = {1, 2, datum::eps, datum::inf, datum::e, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 0.9, 0.89, 0.789, 0.6789, 0.56789, 0.456789, 0.3456789, 0.23456789, 0.123456789};
-  input = join_vert(input, -input);
-  input.save("./input/miscellaneous.sign.mat", raw_ascii);
-
-  input << 0 << 1 << 2 << 10 << 5 << 0 << endr
-      << 0 << 1 << 2 << 50 << -55 << 1 << endr
-      << 0 << 1 << 2 << 20 << 15 << -1 << endr
-      << 0 << 1 << 2 << 40 << -45 << datum::eps << endr
-      << 0 << 1 << 2 << 30 << 30 << -datum::eps << endr;
-  input = join_horiz(input, -input);
-  input.save("./input/statistics.mat", raw_ascii);
-
-  input << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << endr
-      << 5 << 6 << 7 << 2 << 3 << 5 << 6 << 3 << 1 << 4 << 8 << 9 << endr
-      << 16 << 64 << 512 << 128 << 1 << 2 << 4 << 1 << 2 << 4 << 256 << 32 << endr
-      << 0.123456789 << 0.9 << 0.456789 << 0.3456789 << 0.89 << 0.789 << 0.6789 << 0.56789 << 0.23456789 << endr
-      << 1 << 0.00390625 << 0.5 << 0.125 << 0.015625 << 0.0625 << 0.125 << 0.015625 << 0.0625 << 0.25 << 0.03625 << 0.0078125 << endr
-      << 0 << 1 << 0 << -1 << 0 << datum::eps << -1 << 0 << datum::eps << 0 << -datum::eps << 0 << endr
-      << 0 << 1 << 0 << -1 << 0 << datum::inf << -1 << 0 << datum::inf << 0 << -datum::inf << 0 << endr;
-  input = join_vert(input, -input);
-  input.save("./input/series.mat", raw_ascii);
-
-  input << 0 << 1 << 0 << 123 << -1 << 0 << endr
-      << 0 << 1 << 1 << 234 << datum::eps << datum::inf << endr
-      << 0 << 1 << 0 << 345 << -1 << 0 << endr
-      << 0 << 1 << 1 << 456 << -datum::eps << -datum::inf << endr
-      << 0 << 1 << 0 << 567 << -1 << 0 << endr;
-  input = join_horiz(input, -input);
-  input.save("./input/logic.mat", raw_ascii);
+  Mat<double> X = generateKMSMatrix(7, 7);
+  cout << X << endl;
+  cout << X.submat({0, 1}, {2, 3}) << endl;
 
   return EXIT_SUCCESS;
 }
