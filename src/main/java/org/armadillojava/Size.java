7750df2797d34556561953decf3bda7eefaf5443
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-07-04 14:08:14 +0200

    doc: Added javadoc to the class Size
    Fixed #65 (Add comments to the class Size)
    
    Signed-off-by: Sebastian Niemann <niemann@sra.uni-hannover.de>

--- src/main/java/org/armadillojava/Size.java
+++ src/main/java/org/armadillojava/Size.java
@@ -14,7 +14,7 @@
 package org.armadillojava;
 
 /**
- * TODO
+ * Placeholder for the shape of a matrix
  * 
  * @author Sebastian Niemann
  */
@@ -31,18 +31,22 @@ public class Size {
   public final int n_cols;
 
   /**
-   * TODO
+   * Creates a deep copy of {@code size}. 
+   * <p>
+   * The number of rows and columns will be equal to those of {@code size}.
    * 
-   * @param size TODO
+   * @param size any size object
    */
   protected Size(Size size) {
     this(size.n_rows, size.n_cols);
   }
   
   /**
-   * TODO
+   * Creates a size object based on the provided matrix.
+   * <p>
+   * The number of rows will be set to {@code matrix.n_rows} and the number of colums to {@code matrix.n_cols}.
    * 
-   * @param matrix TODO
+   * @param matrix any matrix
    */
   public Size(final Mat matrix) {
     n_rows = matrix.n_rows;
@@ -50,21 +54,23 @@ public class Size {
   }
   
   /**
-   * TODO
+   * Creates a size object based on the provided number of rows and columns.
+   * <p>
+   * The number of rows will be set to {@code n_rows} and the number of colums to {@code n_cols}.
    * 
-   * @param n_rows The number of rows
-   * @param n_cols The number of columns
+   * @param n_rows a non-negative number of rows
+   * @param n_cols a non-negative number of columns
    * 
-   * @throws IllegalArgumentException The number of rows ({@code n_rows}) must be non-negative.
-   * @throws IllegalArgumentException The number of columns ({@code n_cols}) must be non-negative.
+   * @throws IllegalArgumentException The provided number of rows ({@code n_rows}) must be non-negative.
+   * @throws IllegalArgumentException The provided number of columns ({@code n_cols}) must be non-negative.
    */
   public Size(final int n_rows, final int n_cols) {
     if (n_rows < 0) {
-      throw new IllegalArgumentException("The number of rows (" + n_rows + ") must be non-negative.");
+      throw new IllegalArgumentException("The provided number of rows (" + n_rows + ") must be non-negative.");
     }
     
     if (n_cols < 0) {
-      throw new IllegalArgumentException("The number of columns (" + n_cols + ") must be non-negative.");
+      throw new IllegalArgumentException("The provided number of columns (" + n_cols + ") must be non-negative.");
     }
     
     this.n_rows = n_rows;
